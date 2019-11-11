20e99867c310c28eeac969690fe57f13190eff80
Raxhet <50268115+Raxhet@users.noreply.github.com>
2019-11-11 22:47:02 +0500

    newSiteFace

--- RaxhetMvc/Controllers/HomeController.cs
+++ RaxhetMvc/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace RaxhetMvc.Controllers
         {
             return View();
         }
+        public IActionResult Information()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- RaxhetMvc/Program.cs
+++ RaxhetMvc/Program.cs
@@ -27,32 +27,34 @@ namespace RaxhetMvc
                 .UseStartup<Startup>();
     }
 
-    public class DBAccess
-    {
-        SqlConnection con = new SqlConnection("Data Source=wpl22.hosting.reg.ru;User ID=u0790449_Raxhet;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-        public string AddRecord(ErrorViewModel errorViewModel)
-        {
-            try
-            {
-                SqlCommand cmd = new SqlCommand("spMyProcedure", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Name", errorViewModel.Name);
-                cmd.Parameters.AddWithValue("@Surname", errorViewModel.Surname);
-                cmd.Parameters.AddWithValue("@City", errorViewModel.City);
-                con.Open();
-                cmd.ExecuteNonQuery();
-                con.Close();
-                return ("Data save succsess");
-            }
-            catch (Exception ex)
-            {
-                if (con.State == ConnectionState.Open)
-                {
-                    con.Close();
-                }
-                return (ex.Message.ToString());
-            }
-        }
-    }
+
+    //trying connect to DB
+    //public class DBAccess
+    //{
+    //    SqlConnection con = new SqlConnection("Data Source=wpl22.hosting.reg.ru;User ID=u0790449_Raxhet;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    //    public string AddRecord(ErrorViewModel errorViewModel)
+    //    {
+    //        try
+    //        {
+    //            SqlCommand cmd = new SqlCommand("spMyProcedure", con);
+    //            cmd.CommandType = CommandType.StoredProcedure;
+    //            cmd.Parameters.AddWithValue("@Name", errorViewModel.Name);
+    //            cmd.Parameters.AddWithValue("@Surname", errorViewModel.Surname);
+    //            cmd.Parameters.AddWithValue("@City", errorViewModel.City);
+    //            con.Open();
+    //            cmd.ExecuteNonQuery();
+    //            con.Close();
+    //            return ("Data save succsess");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            if (con.State == ConnectionState.Open)
+    //            {
+    //                con.Close();
+    //            }
+    //            return (ex.Message.ToString());
+    //        }
+    //    }
+    //}  
 }
 
